d0d84930cae4ccd8153203083ed1f1625eaac2ff
Macbook Pro <tabraiz.ahmad95@yahoo.com>
2021-03-14 01:07:13 +0400

    project structure

---
+++ app/src/main/java/com/example/popularnyarticles/ui/ArticleListFragment.kt
@@ -0,0 +1,32 @@
+package com.example.popularnyarticles.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.popularnyarticles.R
+
+class ArticleListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ArticleListFragment()
+    }
+
+    private lateinit var viewModel: ArticlesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.article_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ArticlesViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/popularnyarticles/ui/ArticlesViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.popularnyarticles.ui
+
+import androidx.lifecycle.ViewModel
+
+class ArticlesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
