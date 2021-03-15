8cd2216f09802e4c6afce651b4c17bbd52c2b565
Macbook Pro <tabraiz.ahmad95@yahoo.com>
2021-03-15 16:46:18 +0400

    using string placeholder

--- app/src/main/java/com/example/popularnyarticles/ui/article/articledetail/ArticleDetailFragment.kt
+++ app/src/main/java/com/example/popularnyarticles/ui/article/articledetail/ArticleDetailFragment.kt
@@ -8,13 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.popularnyarticles.R
-import com.example.popularnyarticles.databinding.ArticleListFragmentBinding
 import com.example.popularnyarticles.databinding.FragmentArticleDetailBinding
 import com.example.popularnyarticles.extention.getPhotoUrl
 
 class ArticleDetailFragment : Fragment() {
 
-    val args: ArticleDetailFragmentArgs by navArgs()
+    private val args: ArticleDetailFragmentArgs by navArgs()
 
     private lateinit var binding: FragmentArticleDetailBinding
 
@@ -33,8 +32,8 @@ class ArticleDetailFragment : Fragment() {
         article?.let { item->
             binding.articleAbstract.text = item.articleAbstract
             binding.articleTitle.text = item.title
-            binding.publishedDate.text = getString(R.string.published_date)+item.published_date
-            binding.updatedDate.text = getString(R.string.updateed_date)+ item.updated
+            binding.publishedDate.text = getString(R.string.published_date,item.published_date)
+            binding.updatedDate.text = getString(R.string.updated_date, item.updated)
 
             val photoURL = item.getPhotoUrl()
             context?.let {
